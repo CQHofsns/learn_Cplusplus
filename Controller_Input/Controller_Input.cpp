76cc9ad607bcef205480b7337ed35936b9059f58
QUANG HUY CHU <cqhofsns@gmail.com>
2024-06-05 23:54:47 +0900

    Update source code

--- Controller_Input/Controller_Input.cpp
+++ Controller_Input/Controller_Input.cpp
@@ -20,12 +20,32 @@ XINPUT_STATE GetControllerState() {
     return state;
 }
 
+void PrintButtonState(WORD buttons, WORD buttonMask, const char* buttonName) {
+    if (buttons & buttonMask) {
+        std::cout << buttonName << " pressed" << std::endl;
+    }
+}
+
 int main()
 {   
     while (true) {
         XINPUT_STATE state = GetControllerState();
 
-        std::cout << "Button: " << state.Gamepad.wButtons << std::endl;
+        // In state của buttons
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_DPAD_UP, "DPAP UP");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_DPAD_DOWN, "DPAD DOWN");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_DPAD_LEFT, "DPAD LEFT");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_DPAD_RIGHT, "DPAD RIGHT");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_START, "START");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_BACK, "BACK");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_LEFT_THUMB, "LEFT THUMB");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_RIGHT_THUMB, "RIGHT THUMB");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_LEFT_SHOULDER, "LEFT SHOULDER");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_LEFT_SHOULDER, "LEFT SHOULDER");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_A, "A");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_B, "B");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_X, "X");
+        PrintButtonState(state.Gamepad.wButtons, XINPUT_GAMEPAD_Y, "Y");
 
         std::cout << "Left Thumbstick: (" << state.Gamepad.sThumbLX << "," << state.Gamepad.sThumbLY << ")" << std::endl;
         std::cout << "Right Thumbstick: (" << state.Gamepad.sThumbRX << "," << state.Gamepad.sThumbRY << ")" << std::endl;
